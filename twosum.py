f3c16b344246586b89ea3eb1127e07822f912ceb
Santhana Bharathi A <as7842501@gmail.com>
2025-12-11 10:57:26 +0530

    modified the twosum.py

--- twosum.py
+++ twosum.py
@@ -1,1 +1,10 @@
-print("helo")
+class Solution(object):
+    def twoSum(self, nums, target):
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
+
+s1 = Solution()
+print(s1.twoSum(nums=[2, 3, 4, 5, 6], target=9))
